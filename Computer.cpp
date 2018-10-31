ddb532be41d75654da5b44cec0d7a4b874e72896
williamsoccorsi <wsoccors@uvm.edu>
2018-10-31 14:46:51 -0400

    Created sudo dumb algorithm

--- Computer.cpp
+++ Computer.cpp
@@ -1,7 +1,7 @@
 //
 // Created by William  Soccorsi on 10/31/18.
 //
-
+#include <stdlib.h>
 #include "Computer.h"
 
 
@@ -16,16 +16,30 @@ void Computer::setInteligence(enum intelligence i) {
 }
 
 void Computer::computerMove() {
+    bool piecePlacement = false;
 
+     //removes top three in the array
     if (itel == DUMB){
-        
+        while (!piecePlacement){
+            piece p;
+            int randNum = (rand() % 3) + 1;
 
+            if (unplayedPiecies.size() > 3) {
+                int bottomThree = unplayedPieces.size() - (unplayedPieces.size() - 3);
+                placePiece(unplayedPieces[bottomThree]);
+                piecePlacement = true;
+            }
 
+            //places last in the array
+            else {
+               int lastPiece = unplayedPiecies.size() - (unplayedPiecies.size() - 1);
+                placePiece(unplayedPieices[lastPiece]);
+                piecePlacement = true;
+            }
+        }
     }
 
     else if (itel == SMART){
-
-
 
     }
 }
